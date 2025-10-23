eff1123361df5fb210a5ebbb329710dbb17b87c0
Selim1710 <mdselimhossainsuahg@gmail.com>
2025-10-23 17:03:54 +0600

    voucher problem solved

--- resources/views/components/base/footer.blade.php
+++ resources/views/components/base/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="footer bg-white py-4 d-flex flex-lg-column " id="kt_footer">
     <div class=" container-fluid  d-flex flex-column flex-md-row align-items-center justify-content-between">
-        <div class="text-dark order-2 order-md-1 text-center"><span class="text-muted font-weight-bold mr-2">2023 &copy;</span></div>
+        <div class="text-dark order-2 order-md-1 text-center"><span class="text-muted font-weight-bold mr-2">2023 &copy; B2G Soft</span></div>
     </div>
 </div>
