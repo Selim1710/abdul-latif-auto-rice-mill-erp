a9abf516175050023eab6a3c2ff8b85ee101c202
Selim1710 <mdselimhossainsuahg@gmail.com>
2025-11-11 18:19:35 +0600

    udpate

--- Modules/Expense/Http/Controllers/ExpenseItemController.php
+++ Modules/Expense/Http/Controllers/ExpenseItemController.php
@@ -11,58 +11,65 @@ use Modules\Expense\Entities\ExpenseItem;
 use Modules\Expense\Http\Requests\ExpenseItemFormRequest;
 
 
-class ExpenseItemController extends BaseController{
-    public function __construct(ExpenseItem $model){
+class ExpenseItemController extends BaseController
+{
+    public function __construct(ExpenseItem $model)
+    {
         $this->model = $model;
     }
-    public function index(){
-        if(permission('expense-item-access')){
+
+    public function index()
+    {
+        if (permission('expense-item-access')) {
             $setTitle    = __('file.Expense');
             $setSubTitle = __('file.Expense Item');
-            $this->setPageData($setSubTitle,$setSubTitle,'fas fa-money-check-alt',[['name'=>$setTitle,'link'=>'javascript::void();'],['name' => $setSubTitle]]);
+            $this->setPageData($setSubTitle, $setSubTitle, 'fas fa-money-check-alt', [['name' => $setTitle, 'link' => 'javascript::void();'], ['name' => $setSubTitle]]);
             return view('expense::expense-item.index');
-        }else{
+        } else {
             return $this->access_blocked();
         }
     }
-    public function get_datatable_data(Request $request){
-        if($request->ajax() && permission('expense-item-access')){
+
+    public function get_datatable_data(Request $request)
+    {
+        if ($request->ajax() && permission('expense-item-access')) {
             if (!empty($request->name)) {
                 $this->model->setName($request->name);
             }
-            $this->set_datatable_default_properties($request);//set datatable default properties
-            $list = $this->model->getDatatableList();//get table data
+            $this->set_datatable_default_properties($request); //set datatable default properties
+            $list = $this->model->getDatatableList(); //get table data
             $data = [];
             $no   = $request->input('start');
             foreach ($list as $value) {
                 $no++;
                 $action      = '';
-                if(permission('expense-item-edit')){
-                    $action .= ' <a class="dropdown-item edit_data" data-id="' . $value->id . '">'.$this->actionButton('Edit').'</a>';
+                if (permission('expense-item-edit')) {
+                    $action .= ' <a class="dropdown-item edit_data" data-id="' . $value->id . '">' . $this->actionButton('Edit') . '</a>';
                 }
-                if(permission('expense-item-delete')){
-                    $action .= ' <a class="dropdown-item delete_data"  data-id="' . $value->id . '" data-name="' . $value->name . '">'.$this->actionButton('Delete').'</a>';
+                if (permission('expense-item-delete')) {
+                    $action .= ' <a class="dropdown-item delete_data"  data-id="' . $value->id . '" data-name="' . $value->name . '">' . $this->actionButton('Delete') . '</a>';
                 }
                 $row    = [];
                 $row[]  = $no;
                 $row[]  = $value->name;
                 $row[]  = EXPENSE_TYPE_LABEL[$value->expense_type];
                 $row[]  = STATUS_LABEL[$value->status];
-                $row[]  = action_button($action);//custom helper function for action button
+                $row[]  = action_button($action); //custom helper function for action button
                 $data[] = $row;
             }
-            return $this->datatable_draw($request->input('draw'),$this->model->count_all(), $this->model->count_filtered(), $data);
-        }else{
+            return $this->datatable_draw($request->input('draw'), $this->model->count_all(), $this->model->count_filtered(), $data);
+        } else {
             return response()->json($this->unauthorized());
         }
     }
-    public function store_or_update_data(ExpenseItemFormRequest $request){
-        if($request->ajax() && permission('expense-item-add')){
+    public function store_or_update_data(ExpenseItemFormRequest $request)
+    {
+        if ($request->ajax() && permission('expense-item-add')) {
             DB::beginTransaction();
-            try{
+            try {
                 $collection   = collect($request->all())->merge(['status' => 1]);
-                $collection   = $this->track_data($collection,$request->update_id);
-                $result       = $this->model->updateOrCreate(['id'=>$request->update_id],$collection->all());
+                $collection   = $this->track_data($collection, $request->update_id);
+                $result       = $this->model->updateOrCreate(['id' => $request->update_id], $collection->all());
                 ChartOfHead::updateOrCreate(
                     [
                         'expense_item_id'  => $result->id,
@@ -73,41 +80,43 @@ class ExpenseItemController extends BaseController{
                         'type'             => 2,
                         'head_id'          => $request->expense_type == 1 ? 19 : 20,
                         'expense_item_id'  => $result->id,
-                        'name'             => $request->name.'('.EXPENSE_TYPE_VALUE[$request->expense_type].')',
+                        'name'             => $request->name . '(' . EXPENSE_TYPE_VALUE[$request->expense_type] . ')',
                         'classification'   => 4
                     ]
                 );
                 $output       = $this->store_message($result, $request->update_id);
                 DB::commit();
-            }catch(Exception $e){
+            } catch (Exception $e) {
                 DB::rollBack();
-                $output = ['status' => 'error' , 'message' => $e->getMessage()];
+                $output = ['status' => 'error', 'message' => $e->getMessage()];
             }
             return response()->json($output);
-        }else{
+        } else {
             return response()->json($this->unauthorized());
         }
     }
-    public function edit(Request $request){
-        if($request->ajax()){
-            if(permission('expense-item-edit')){
+    public function edit(Request $request)
+    {
+        if ($request->ajax()) {
+            if (permission('expense-item-edit')) {
                 $data   = $this->model->findOrFail($request->id);
                 $output = $this->data_message($data);
-            }else{
+            } else {
                 $output = $this->unauthorized();
             }
             return response()->json($output);
-        }else{
+        } else {
             return response()->json($this->unauthorized());
         }
     }
-    public function delete(Request $request){
-        if($request->ajax() && permission('expense-item-delete')){
+    public function delete(Request $request)
+    {
+        if ($request->ajax() && permission('expense-item-delete')) {
             $result   = $this->model->findOrFail($request->id)->delete();
             $output   = $this->delete_message($result);
             $this->model->flushCache();
             return response()->json($output);
-        }else{
+        } else {
             return response()->json($this->unauthorized());
         }
     }
--- Modules/Transport/Http/Controllers/TransportController.php
+++ Modules/Transport/Http/Controllers/TransportController.php
@@ -22,25 +22,30 @@ use Modules\Transport\Entities\TransportDetail;
 use Modules\Transport\Entities\Truck;
 use Modules\Transport\Http\Requests\TransportRequestForm;
 
-class TransportController extends BaseController{
+class TransportController extends BaseController
+{
     private const t = 'TRANSPORT';
-    public function __construct(Transport $model){
+    public function __construct(Transport $model)
+    {
         $this->model = $model;
     }
-    public function index(){
+    public function index()
+    {
         if (permission('transport-access')) {
             $setTitle = __('file.Transport Manage');
             $this->setPageData($setTitle, $setTitle, 'fas fa-list-alt', [['name' => $setTitle]]);
             $data = [
                 'trucks'  => Truck::all()
             ];
-            return view('transport::transport.index',$data);
+            return view('transport::transport.index', $data);
         } else {
             return $this->access_blocked();
         }
     }
-    public function get_datatable_data(Request $request){
-        if($request->ajax() && permission('transport-access')){
+
+    public function get_datatable_data(Request $request)
+    {
+        if ($request->ajax() && permission('transport-access')) {
             if (!empty($request->voucher_no)) {
                 $this->model->setVoucherNo($request->voucher_no);
             }
@@ -54,17 +59,17 @@ class TransportController extends BaseController{
             foreach ($list as $value) {
                 $no++;
                 $action = '';
-                if(permission('transport-edit')){
-                    $action .= ' <a class="dropdown-item" href="'.route("transport.edit",$value->id).'">'.$this->actionButton('Edit').'</a>';
+                if (permission('transport-edit')) {
+                    $action .= ' <a class="dropdown-item" href="' . route("transport.edit", $value->id) . '">' . $this->actionButton('Edit') . '</a>';
                 }
-                if(permission('transport-view')){
-                    $action .= ' <a class="dropdown-item view_data" href="'.route("transport.view",$value->id).'">'.$this->actionButton('View').'</a>';
+                if (permission('transport-view')) {
+                    $action .= ' <a class="dropdown-item view_data" href="' . route("transport.view", $value->id) . '">' . $this->actionButton('View') . '</a>';
                 }
-                if(permission('transport-change-status') && $value->status != 1){
-                    $action .= ' <a class="dropdown-item change_status"  data-id="' . $value->id . '" data-name="' . $value->invoice_no . '" data-status="'.$value->status.'">'.$this->actionButton('Change Status').'</a>';
+                if (permission('transport-change-status') && $value->status != 1) {
+                    $action .= ' <a class="dropdown-item change_status"  data-id="' . $value->id . '" data-name="' . $value->invoice_no . '" data-status="' . $value->status . '">' . $this->actionButton('Change Status') . '</a>';
                 }
-                if(permission('transport-delete')){
-                    $action .= ' <a class="dropdown-item delete_data"  data-id="' . $value->id . '" data-name="' . $value->invoice_no . '">'.$this->actionButton('Delete').'</a>';
+                if (permission('transport-delete')) {
+                    $action .= ' <a class="dropdown-item delete_data"  data-id="' . $value->id . '" data-name="' . $value->invoice_no . '">' . $this->actionButton('Delete') . '</a>';
                 }
                 $row    = [];
                 $row[]  = $no;
@@ -75,20 +80,22 @@ class TransportController extends BaseController{
                 $row[]  = $value->truck->truck_no;
                 $row[]  = $value->driver_name;
                 $row[]  = $value->rent_name;
-                $row[]  = number_format($value->rent_amount,2);
-                $row[]  = number_format($value->total_expense,2);
-                $row[]  = number_format($value->income,2);
+                $row[]  = number_format($value->rent_amount, 2);
+                $row[]  = number_format($value->total_expense, 2);
+                $row[]  = number_format($value->income, 2);
                 $row[]  = VOUCHER_APPROVE_STATUS_LABEL[$value->status];
                 $row[]  = $value->created_by;
-                $row[]  = action_button($action);//custom helper function for action button
+                $row[]  = action_button($action); //custom helper function for action button
                 $data[] = $row;
             }
-            return $this->datatable_draw($request->input('draw'),$this->model->count_all(), $this->model->count_filtered(), $data);
-        }else{
+            return $this->datatable_draw($request->input('draw'), $this->model->count_all(), $this->model->count_filtered(), $data);
+        } else {
             return response()->json($this->unauthorized());
         }
     }
-    public function create(){
+
+    public function create()
+    {
         if (permission('transport-access')) {
             $setTitle    = __('file.Transports');
             $setSubTitle = __('file.Transports Add');
@@ -97,81 +104,85 @@ class TransportController extends BaseController{
                 'parties'        => Party::all(),
                 'invoice_no'     => self::t . '-' . round(microtime(true) * 1000),
                 'trucks'         => Truck::allTrucks(),
-                'expenseItems'   => ExpenseItem::OtherExpense()->get()
+                'expenseItems'   => ExpenseItem::TransportExpense()->get()
             ];
             return view('transport::transport.create', $data);
         } else {
             return $this->access_blocked();
         }
     }
-    public function store(TransportRequestForm $request){
-        if($request->ajax() && permission('transport-add')){
+    public function store(TransportRequestForm $request)
+    {
+        if ($request->ajax() && permission('transport-add')) {
             DB::beginTransaction();
-            try{
+            try {
                 $transport  = [];
-                $collection = collect($request->all())->except('_token','transport')->merge(['status' => 3 ,'created_by' => auth()->user()->name]);
+                $collection = collect($request->all())->except('_token', 'transport')->merge(['status' => 3, 'created_by' => auth()->user()->name]);
                 $result     = $this->model->create($collection->all());
-                if($request->has('transport')){
-                    foreach ($request->transport as $value){
-                        if(!empty($value['expense_item_id']) && !empty($value['amount'])){
+                if ($request->has('transport')) {
+                    foreach ($request->transport as $value) {
+                        if (!empty($value['expense_item_id']) && !empty($value['amount'])) {
                             $transport[] = [
-                              'expense_item_id' => $value['expense_item_id'],
-                              'amount'          => $value['amount']
+                                'expense_item_id' => $value['expense_item_id'],
+                                'amount'          => $value['amount']
                             ];
                         }
                     }
                 }
                 $result->transportDetails()->attach($transport);
-                $output = ['status' => 'success' , 'message' => $this->responseMessage('Data Saved')];
+                $output = ['status' => 'success', 'message' => $this->responseMessage('Data Saved')];
                 DB::commit();
-            }catch (Exception $e){
+            } catch (Exception $e) {
                 DB::rollBack();
-                $output = ['status' => 'error' , 'message' => $e->getMessage()];
+                $output = ['status' => 'error', 'message' => $e->getMessage()];
             }
             return response()->json($output);
-        }else{
+        } else {
             return response()->json($this->unauthorized());
         }
     }
-    public function show(int $id){
-        if(permission('transport-view')){
+    public function show(int $id)
+    {
+        if (permission('transport-view')) {
             $setTitle    = __('file.Transport');
             $setSubTitle = __('file.Transport Details');
-            $this->setPageData($setSubTitle,$setSubTitle,'fas fa-file',[['name'=>$setTitle,'link' => route('transport')],['name' => $setTitle]]);
+            $this->setPageData($setSubTitle, $setSubTitle, 'fas fa-file', [['name' => $setTitle, 'link' => route('transport')], ['name' => $setTitle]]);
             $data = [
-                'details' => $this->model->with('truck','party','transportDetailsList.expenseItem')->findOrFail($id),
+                'details' => $this->model->with('truck', 'party', 'transportDetailsList.expenseItem')->findOrFail($id),
             ];
             return view('transport::transport.details', $data);
-        }else{
+        } else {
             return $this->access_blocked();
         }
     }
-    public function edit(int $id){
-        if(permission('transport-edit')){
+    public function edit(int $id)
+    {
+        if (permission('transport-edit')) {
             $setTitle    = __('file.Transport');
             $setSubTitle = __('file.Transport Details');
-            $this->setPageData($setSubTitle,$setSubTitle,'fas fa-file',[['name'=>$setTitle,'link' => route('transport')],['name' => $setTitle]]);
+            $this->setPageData($setSubTitle, $setSubTitle, 'fas fa-file', [['name' => $setTitle, 'link' => route('transport')], ['name' => $setTitle]]);
             $data = [
-                'details'        => $this->model->with('truck','party','transportDetailsList')->findOrFail($id),
+                'details'        => $this->model->with('truck', 'party', 'transportDetailsList')->findOrFail($id),
                 'parties'        => Party::all(),
                 'trucks'         => Truck::allTrucks(),
                 'expenseItems'   => ExpenseItem::OtherExpense()->get()
             ];
             return view('transport::transport.edit', $data);
-        }else{
+        } else {
             return $this->access_blocked();
         }
     }
-    public function update(TransportRequestForm $request){
-        if($request->ajax() && permission('transport-edit')){
+    public function update(TransportRequestForm $request)
+    {
+        if ($request->ajax() && permission('transport-edit')) {
             DB::beginTransaction();
-            try{
+            try {
                 $transport  = [];
-                $collection = collect($request->all())->except('_token','transport')->merge(['modified' => auth()->user()->name]);
+                $collection = collect($request->all())->except('_token', 'transport')->merge(['modified' => auth()->user()->name]);
                 $result     = $this->model->findOrFail($request->update_id);
-                if($request->has('transport')){
-                    foreach ($request->transport as $value){
-                        if(!empty($value['expense_item_id']) && !empty($value['amount'])){
+                if ($request->has('transport')) {
+                    foreach ($request->transport as $value) {
+                        if (!empty($value['expense_item_id']) && !empty($value['amount'])) {
                             $transport[Str::random(5)] = [
                                 'expense_item_id' => $value['expense_item_id'],
                                 'amount'          => $value['amount']
@@ -182,75 +193,78 @@ class TransportController extends BaseController{
                 $result->update($collection->all());
                 $result->transportDetails()->sync($transport);
                 $result->touch();
-                $output = ['status' => 'success' , 'message' => $this->responseMessage('Data Saved')];
+                $output = ['status' => 'success', 'message' => $this->responseMessage('Data Saved')];
                 DB::commit();
-            }catch (Exception $e){
+            } catch (Exception $e) {
                 DB::rollBack();
-                $output = ['status' => 'error' , 'message' => $e->getMessage()];
+                $output = ['status' => 'error', 'message' => $e->getMessage()];
             }
             return response()->json($output);
-        }else{
+        } else {
             return response()->json($this->unauthorized());
         }
     }
-    public function changeStatus(Request $request){
-        if($request->ajax() && permission('transport-change-status')){
+    public function changeStatus(Request $request)
+    {
+        if ($request->ajax() && permission('transport-change-status')) {
             DB::beginTransaction();
-            try{
+            try {
                 $transport      = $this->model->with('transportDetailsList')->findOrFail($request->id);
                 $transportCohId = ChartOfHead::firstWhere(['truck_id' => $transport->truck_id]);
-                abort_if($transport->status == 1,404);
-                if($transport->party_type == 1){
-                    $party      = ChartOfHead::firstWhere(['master_head' => 1,'party_id' => $transport->party_id]);
+                abort_if($transport->status == 1, 404);
+                if ($transport->party_type == 1) {
+                    $party      = ChartOfHead::firstWhere(['master_head' => 1, 'party_id' => $transport->party_id]);
                     $cohId      = $party->id;
                     $name       = $party->name;
-                }else{
+                } else {
                     $cohId      = 22;
                     $name       = 'Walking Customer';
                 }
-                foreach ($transport->transportDetailsList as $value){
-                    $expenseItemCohId = ChartOfHead::firstWhere(['master_head' => 7,'expense_item_id' => $value->expense_item_id]);
-                    $narration        = 'Transport Expense Cost Invoice No -'.$transport->invoice_no;
-                    $this->balanceDebit($expenseItemCohId->id,$transport->invoice_no,$narration,$transport->date,$value->amount);
+                foreach ($transport->transportDetailsList as $value) {
+                    $expenseItemCohId = ChartOfHead::firstWhere(['master_head' => 7, 'expense_item_id' => $value->expense_item_id]);
+                    $narration        = 'Transport Expense Cost Invoice No -' . $transport->invoice_no;
+                    $this->balanceDebit($expenseItemCohId->id, $transport->invoice_no, $narration, $transport->date, $value->amount);
                 }
                 $transport->update([
-                   'status'     => 1
+                    'status'     => 1
                 ]);
-                $partyNarration  = $name.' Transport Location '.$transport->rent_name.' Receivable Amount '.$transport->rent_amount;
-                $this->balanceDebit($cohId,$transport->invoice_no,$partyNarration,$transport->date,$transport->rent_amount);
-                $this->balanceDebit($transportCohId->id,$transport->invoice_no,$partyNarration,$transport->date,$transport->rent_amount);
-                $this->balanceCredit(24,$transport->invoice_no,$partyNarration,$transport->date,$transport->total_expense);
-                $output = ['status' => 'success' , 'message' => 'Transport Status Change Successfully'];
+                $partyNarration  = $name . ' Transport Location ' . $transport->rent_name . ' Receivable Amount ' . $transport->rent_amount;
+                $this->balanceDebit($cohId, $transport->invoice_no, $partyNarration, $transport->date, $transport->rent_amount);
+                $this->balanceDebit($transportCohId->id, $transport->invoice_no, $partyNarration, $transport->date, $transport->rent_amount);
+                $this->balanceCredit(24, $transport->invoice_no, $partyNarration, $transport->date, $transport->total_expense);
+                $output = ['status' => 'success', 'message' => 'Transport Status Change Successfully'];
                 DB::commit();
-            }catch(Exception $e){
+            } catch (Exception $e) {
                 DB::rollBack();
-                $output = ['status' => 'error' , 'message' => $e->getMessage()];
+                $output = ['status' => 'error', 'message' => $e->getMessage()];
             }
             return response()->json($output);
-        }else{
+        } else {
             return response()->json($this->unauthorized());
         }
     }
-    public function delete(Request $request){
-        if($request->ajax() && permission('transport-delete')){
+    public function delete(Request $request)
+    {
+        if ($request->ajax() && permission('transport-delete')) {
             DB::beginTransaction();
-            try{
+            try {
                 $transport = $this->model->with('transportDetailsList')->findOrFail($request->id);
-                abort_if($transport->status == 1,404);
+                abort_if($transport->status == 1, 404);
                 $transport->transportDetailsList()->delete();
                 $transport->delete();
-                $output = ['status' => 'success' , 'message' => 'Data Deleted Successfully'];
+                $output = ['status' => 'success', 'message' => 'Data Deleted Successfully'];
                 DB::commit();
-            }catch(Exception $e){
+            } catch (Exception $e) {
                 DB::rollBack();
-                $output = ['status' => 'error' , 'message' => $e->getMessage()];
+                $output = ['status' => 'error', 'message' => $e->getMessage()];
             }
             return response()->json($output);
-        }else{
+        } else {
             return response()->json($this->unauthorized());
         }
     }
-    public function balanceDebit($cohId,$invoiceNo,$narration,$date,$paidAmount){
+    public function balanceDebit($cohId, $invoiceNo, $narration, $date, $paidAmount)
+    {
         Transaction::create([
             'chart_of_head_id' => $cohId,
             'date'             => $date,
@@ -264,7 +278,8 @@ class TransportController extends BaseController{
             'created_by'       => auth()->user()->name,
         ]);
     }
-    public function balanceCredit($cohId,$invoiceNo,$narration,$date,$paidAmount){
+    public function balanceCredit($cohId, $invoiceNo, $narration, $date, $paidAmount)
+    {
         Transaction::create([
             'chart_of_head_id' => $cohId,
             'date'             => $date,
